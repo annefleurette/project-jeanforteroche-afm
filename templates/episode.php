3be7dd1fc6fb84f940499db911a18deff231c00e
annefleur <af.marchat@gmail.com>
2020-04-12 17:27:12 +0200

    Episodes management
    - Case with zero episode
    - Debug of pagination

--- templates/episode.php
+++ templates/episode.php
@@ -22,93 +22,100 @@
                     {
                         die('Erreur : '.$e->getMessage());
                     }
-                    // Récupération de l'épisode
-                    $req = $bdd->prepare('SELECT episode_title, episode_content FROM episodes WHERE episode_number = ?');
-                    $req->execute(array(htmlspecialchars($_GET['number'])));
-                    $episode_unitary = $req->fetch();
-                    //Récupération du nombre d'épisodes
-                    $pagination = $bdd->query('SELECT COUNT(*) AS numberEpisodes FROM episodes WHERE episode_status = "published"');
-                    $reading = $pagination->fetch();
-                    $reading_pages = $reading['numberEpisodes'];
-                    if (!empty($episode_unitary)) {
-                        $episode_current = intval(htmlspecialchars($_GET['number']));
-                        $episode_before = $episode_current - 1;
-                        $episode_next = $episode_current + 1;
-                        ?>
-                        <h1>BILLET SIMPLE POUR L'ALASKA</h1>
-                        <h2>Episode n°<?php echo htmlspecialchars($_GET['number']);?> : <?php echo $episode_unitary['episode_title']; ?></h2>
-                        <p><?php echo $episode_unitary['episode_content']; ?></p>
-                        <?php // Affichage des boutons épisodes précédents/suivants
-                        if ($episode_current <= 1) {
-                        ?>
-                        <a href="episode.php?number=<?php echo $episode_next; ?>">Episode suivant</a>
-                        <?php    
-                        }else if ($episode_current >= $reading_pages){
-                        ?>
-                        <a href="episode.php?number=<?php echo $episode_before; ?>">Episode précédent</a>
-                        <?php
-                        }else{
-                        ?>
-                        <a href="episode.php?number=<?php echo $episode_before; ?>">Episode précédent</a>
-                        <a href="episode.php?number=<?php echo $episode_next; ?>">Episode suivant</a>
-                        <?php
-                        }
-                        ?>
-                        <h2>Commentaires</h2>
-                        <?php // Récupération des commentaires
-                        $req_idepisode = $bdd->prepare('SELECT id FROM episodes WHERE episode_number = ?'); // On commence par récupérer l'id de l'épisode
-                        $req_idepisode->execute(array(htmlspecialchars($_GET['number'])));
-                        $exe_idepisode = $req_idepisode->fetch(PDO::FETCH_COLUMN);
-                        $comment_recup = $bdd->prepare('SELECT m.pseudo pseudo_members, c.comment comment_comments, c.id id_comments, DATE_FORMAT(c.date_comment, \'%d/%m/%Y à %Hh%imin%ss\') AS date_comment_fr FROM members m INNER JOIN comments c ON m.id = c.id_pseudo WHERE c.id_episode = ? ORDER BY date_comment');
-                        $comment_recup->execute(array(htmlspecialchars($exe_idepisode)));
-                        $comments = $comment_recup->fetchAll();
-                        $comment_recup->closeCursor();
-                        $nbcomments = count($comments);
-                        if($nbcomments > 0) {
-                            foreach ($comments as $comment_data){
-                                ?>
-                                <p><?php echo $comment_data['pseudo_members']; ?> le <?php echo $comment_data['date_comment_fr']; ?></p>
-                                <p><?php echo nl2br($comment_data['comment_comments']); ?></p>
-                                <form action="alert_post.php?id=<?php echo $comment_data['id_comments'];?>" method="post">
-                                    <input type="submit" value="Signaler">
-                                </form>
-                                <?php
-                            }
-                        }else{
+                        // Récupération de l'épisode
+                        $req = $bdd->prepare('SELECT episode_title, episode_content FROM episodes WHERE episode_number = ?');
+                        $req->execute(array(htmlspecialchars($_GET['number'])));
+                        $episode_unitary = $req->fetch();
+                        //Récupération du nombre d'épisodes
+                        $pagination = $bdd->query('SELECT COUNT(*) AS numberEpisodes FROM episodes WHERE episode_status = "published"');
+                        $reading = $pagination->fetch();
+                        $reading_pages = $reading['numberEpisodes'];
+                        if (!empty($episode_unitary)) {
+                            $episode_current = intval(htmlspecialchars($_GET['number']));
+                            $episode_before = $episode_current - 1;
+                            $episode_next = $episode_current + 1;
                             ?>
-                            <p>Pas de commentaire</p>
-                            <?php     
-                        } // Laisser un commentaire
-                        ?>
-                        <h2>Laisser un commentaire</h2>
-                        <?php
-                        if(!isset($_SESSION['pseudo'])) {
-                        ?>
-                        <p>Vous devez être connecté(e) pour laisser un commentaire. <a href="subscription.php">S'inscrire</a> ou <a href="login.php">se connecter</a>.
-                        <?php
-                        }
-                        ?>
-                        <form action="comment_post.php?number=<?php echo htmlspecialchars($_GET['number']);?>" method="post">
-                            <p>
-                                <label for="comment">Commentaire :</label><br />
-                                <textarea id="comment" name="comment" rows="5" cols="33" minlength = "4" required></textarea>
-                            </p>
-                            <p>
-                                <input type="submit" value="Envoyer">
-                            </p>
-                        </form>
-                    <?php
-                    }else{
-                    ?>
+                            <h1>BILLET SIMPLE POUR L'ALASKA</h1>
+                            <h2>Episode n°<?php echo htmlspecialchars($_GET['number']);?> : <?php echo $episode_unitary['episode_title']; ?></h2>
+                            <p><?php echo $episode_unitary['episode_content']; ?></p>
+                            <?php // Affichage des boutons épisodes précédents/suivants
+                            if($episode_current <= 1){
+                                if($reading_pages == 1){
+                                    ?>
+                                    <p>Un seul épisode publié pour le moment !</p>
+                                    <?php  
 
-                        <p>L'épisode que vous cherchez n'existe pas !</p>
+                                }else{
+                                    ?>
+                                    <a href="episode.php?number=<?php echo $episode_next; ?>">Episode suivant</a>
+                                    <?php 
+                                }   
+                            }elseif($episode_current >= $reading_pages){
+                                ?>
+                                <a href="episode.php?number=<?php echo $episode_before; ?>">Episode précédent</a>
+                                <?php
+                            }else{
+                                ?>
+                                <a href="episode.php?number=<?php echo $episode_before; ?>">Episode précédent</a>
+                                <a href="episode.php?number=<?php echo $episode_next; ?>">Episode suivant</a>
+                                <?php 
+                            }
+                            ?>
+                            <h2>Commentaires</h2>
+                            <?php // Récupération des commentaires
+                            $req_idepisode = $bdd->prepare('SELECT id FROM episodes WHERE episode_number = ?'); // On commence par récupérer l'id de l'épisode
+                            $req_idepisode->execute(array(htmlspecialchars($_GET['number'])));
+                            $exe_idepisode = $req_idepisode->fetch(PDO::FETCH_COLUMN);
+                            $comment_recup = $bdd->prepare('SELECT m.pseudo pseudo_members, c.comment comment_comments, c.id id_comments, DATE_FORMAT(c.date_comment, \'%d/%m/%Y à %Hh%imin%ss\') AS date_comment_fr FROM members m INNER JOIN comments c ON m.id = c.id_pseudo WHERE c.id_episode = ? ORDER BY date_comment');
+                            $comment_recup->execute(array(htmlspecialchars($exe_idepisode)));
+                            $comments = $comment_recup->fetchAll();
+                            $comment_recup->closeCursor();
+                            $nbcomments = count($comments);
+                            if($nbcomments > 0) {
+                                foreach ($comments as $comment_data){
+                                    ?>
+                                    <p><?php echo $comment_data['pseudo_members']; ?> le <?php echo $comment_data['date_comment_fr']; ?></p>
+                                    <p><?php echo nl2br($comment_data['comment_comments']); ?></p>
+                                    <form action="alert_post.php?id=<?php echo $comment_data['id_comments'];?>" method="post">
+                                        <input type="submit" value="Signaler">
+                                    </form>
+                                    <?php
+                                }
+                            }else{
+                                ?>
+                                <p>Pas de commentaire</p>
+                                <?php     
+                            } // Laisser un commentaire
+                            ?>
+                            <h2>Laisser un commentaire</h2>
+                            <?php
+                            if(!isset($_SESSION['pseudo'])) {
+                            ?>
+                            <p>Vous devez être connecté(e) pour laisser un commentaire. <a href="subscription.php">S'inscrire</a> ou <a href="login.php">se connecter</a>.
+                            <?php
+                            }
+                            ?>
+                            <form action="comment_post.php?number=<?php echo htmlspecialchars($_GET['number']);?>" method="post">
+                                <p>
+                                    <label for="comment">Commentaire :</label><br />
+                                    <textarea id="comment" name="comment" rows="5" cols="33" minlength = "4" required></textarea>
+                                </p>
+                                <p>
+                                    <input type="submit" value="Envoyer">
+                                </p>
+                            </form>
                         <?php
-                    }
-                        // Fin de la requête
-                        $req_idepisode->closeCursor();
-                        $req->closeCursor();
-                        $pagination->closeCursor(); 
-                    ?>
+                        }else{
+                        ?>
+
+                            <p>L'épisode que vous cherchez n'existe pas !</p>
+                            <?php
+                        }
+                            // Fin de la requête
+                            $req_idepisode->closeCursor();
+                            $req->closeCursor();
+                            $pagination->closeCursor();
+                        ?>
             </section>
             <?php include("footer.php");?>
         </div>
--- templates/episodes.php
+++ templates/episodes.php
@@ -28,48 +28,58 @@
                     {
                     die('Erreur : '.$e->getMessage());
                     }
-                    // On programme la numérotation de page
-                    $pagination = $bdd->query('SELECT COUNT(*) AS numberEpisodes FROM episodes WHERE episode_status = "published"');
-                    $reading = $pagination->fetch();
-                    $reading_pages = ceil(($reading['numberEpisodes'])/3);
-                    if (isset($_GET['page']) && ($_GET['page'] > 0))
-                    {
-                        $page = htmlspecialchars($_GET['page']);
+                    // On vérifie s'il y a des épisodes ou pas
+                    $check = $bdd->query('SELECT id FROM episodes');
+                    $check_result = $check->fetchAll(PDO::FETCH_COLUMN);
+                    $check->closeCursor();
+                    if(empty($check_result)){
+                        ?>
+                        <p>Jean Forteroche n'a pas encore publié d'épisode de son roman Billet pour l'Alaska</p>
+                        <?php
                     }else{
-                        $page = 1;
-                    }
-                    if ($page > $reading_pages) {
-                        $page = $reading_pages;
-                    }
-                    // On récupère les épisodes
-                    $req = $bdd->query('SELECT episode_number, episode_title, episode_content FROM episodes WHERE episode_status = "published" ORDER BY episode_number LIMIT '. ($page-1)*3 .',3');
-                    $episode_all = $req->fetchAll();
-                    $req->closeCursor();
-                    $nbepisode_all = count($episode_all);
-                        if($nbepisode_all > 0) {
-                            foreach ($episode_all as $episodes_all){
-                            ?>
-                                <ul> <!-- On affiche les épisodes -->
-                                    <li>
-                                        <article>
-                                            <p>Episode n°<?php echo $episodes_all['episode_number']; ?> :</p>
-                                            <h1><?php echo $episodes_all['episode_title']; ?></h2>
-                                            <a href="episode.php?number=<?php echo $episodes_all['episode_number']; ?>" class="btn btn__read">Lire l'épisode</a>
-                                        </article>
-                                    </li>
-                                </ul>
-                            <?php
-                            }
-                            //Affichage des pages avec 3 épisodes par page
-                            for($pages=1 ; $pages<= $reading_pages ; $pages++){
-                                echo '<a href="episode.php?page='. $pages . '" style="margin:2px;">' . $pages . '</a>';
-                            }
+                        // On programme la numérotation de page
+                        $pagination = $bdd->query('SELECT COUNT(*) AS numberEpisodes FROM episodes WHERE episode_status = "published"');
+                        $reading = $pagination->fetch();
+                        $reading_pages = ceil(($reading['numberEpisodes'])/3);
+                        if (isset($_GET['page']) && ($_GET['page'] > 0))
+                        {
+                            $page = htmlspecialchars($_GET['page']);
                         }else{
-                            ?>
-                            <p>Pas d'épisode publié</p>
-                            <?php
+                            $page = 1;
                         }
-                    $pagination->closeCursor();
+                        if ($page > $reading_pages) {
+                            $page = $reading_pages;
+                        }
+                        // On récupère les épisodes
+                        $req = $bdd->query('SELECT episode_number, episode_title, episode_content FROM episodes WHERE episode_status = "published" ORDER BY episode_number LIMIT '. ($page-1)*3 .',3');
+                        $episode_all = $req->fetchAll();
+                        $req->closeCursor();
+                        $nbepisode_all = count($episode_all);
+                            if($nbepisode_all > 0) {
+                                foreach ($episode_all as $episodes_all){
+                                ?>
+                                    <ul> <!-- On affiche les épisodes -->
+                                        <li>
+                                            <article>
+                                                <p>Episode n°<?php echo $episodes_all['episode_number']; ?> :</p>
+                                                <h1><?php echo $episodes_all['episode_title']; ?></h2>
+                                                <a href="episode.php?number=<?php echo $episodes_all['episode_number']; ?>" class="btn btn__read">Lire l'épisode</a>
+                                            </article>
+                                        </li>
+                                    </ul>
+                                <?php
+                                }
+                                //Affichage des pages avec 3 épisodes par page
+                                for($pages=1 ; $pages<= $reading_pages ; $pages++){
+                                    echo '<a href="episode.php?page='. $pages . '" style="margin:2px;">' . $pages . '</a>';
+                                }
+                            }else{
+                                ?>
+                                <p>Pas d'épisode publié</p>
+                                <?php
+                            }
+                        $pagination->closeCursor();
+                    }
                 ?>
             </section>
             <?php include("footer.php"); ?>
