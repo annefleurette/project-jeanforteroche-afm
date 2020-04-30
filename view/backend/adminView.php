29643337175f04b938e9d1f7f58f9fc651d3e1ec
annefleur <af.marchat@gmail.com>
2020-04-30 21:01:04 +0200

    CSS - Backend
    AdminView

--- view/backend/adminView.php
+++ view/backend/adminView.php
@@ -13,57 +13,59 @@ ob_start();
 	    if($nbepisode_published > 0)
 	    {
 	    ?>
-	    	<ul> <!-- On affiche les épisodes publiés -->
-	    	<?php
-	        foreach ($published as $published_episode)
-	       	{
-	        ?>
-	        	<li>
-	                <article>
-	                    <p>Episode n°<?php echo $published_episode['episode_number']; ?> :</p>
-	                    <h4><?php echo $published_episode['episode_title']; ?></h4>
-	                    <ul>
-	                        <!-- Lire l'épisode -->
-	                        <li><a href="index.php?action=episode&amp;number=<?php echo $published_episode['episode_number']; ?>" class="btn btn__admin">Lire</a></li>
-	                        <!-- Modifier l'épisode -->
-	                        <li><a href="index.php?action=update_episode&amp;id=<?php echo $published_episode['id']; ?>" class="btn btn__admin">Modifier</a></li>
-	                        <!-- Supprimer l'épisode avec demande de confirmation - On ne peut supprimer que le dernier épisode publié -->
-	                        <?php
-	                        if($published_episode['episode_number'] == $nbepisode_published)
-	                        {
-	                        ?>
-	                            <li><button type="button" data-toggle="modal" data-target="#Modal">Supprimer</button></li>
-	                                <!-- Modal -->
-	                                <div class="modal fade" id="Modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-	                                    <div class="modal-dialog" role="document">
-	                                        <div class="modal-content">
-	                                            <div class="modal-header">
-	                                                <h5 class="modal-title" id="exampleModalLabel">Confirmation de suppression</h5>
-	                                                <button type="button" class="close" data-dismiss="modal" aria-label="close">
-	                                                	<span aria-hidden="true">&times;</span>
-	                                                </button>
-	                                            </div>
-	                                            <div class="modal-body">
-	                                                <p>Êtes-vous sûr(e) de vouloir supprimer cet épisode ?</p>
-	                                            </div>
-	                                            <div class="modal-footer">
-	                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
-	                                                <a href="index.php?action=delete_episode&amp;id=<?php echo $published_episode['id']; ?>" class="btn btn__admin">Confirmer</a>
-	                                            </div>
-	                                        </div>
-	                                    </div>
-	                                </div>
-	                        <?php
-	                       	}
-	                        ?>
-	                    </ul> 
-	                </article>
-	            </li>                
-	        <?php
-	        }
-	        ?>
-	        </ul>
-	        <?php
+			<div class="novel-episodes__list">
+				<ul> <!-- On affiche les épisodes publiés -->
+				<?php
+				foreach ($published as $published_episode)
+				{
+				?>
+					<li class="row">
+						<article class="col-md-8 col-sm-10 col-xs-12">
+							<p>Episode n°<?php echo $published_episode['episode_number']; ?></p>
+							<h4><?php echo $published_episode['episode_title']; ?></h4>
+							<ul class="novel-episodes__list__buttons">
+								<!-- Lire l'épisode -->
+								<li><a class="btn btn__read" href="index.php?action=episode&amp;number=<?php echo $published_episode['episode_number']; ?>" class="btn btn__admin">Lire</a></li>
+								<!-- Modifier l'épisode -->
+								<li><a class="btn btn__read" href="index.php?action=update_episode&amp;id=<?php echo $published_episode['id']; ?>" class="btn btn__admin">Modifier</a></li>
+								<!-- Supprimer l'épisode avec demande de confirmation - On ne peut supprimer que le dernier épisode publié -->
+								<?php
+								if($published_episode['episode_number'] == $nbepisode_published)
+								{
+								?>
+									<li><button class="btn btn__alert" type="button" data-toggle="modal" data-target="#Modal">Supprimer</button></li>
+										<!-- Modal -->
+										<div class="modal fade" id="Modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+											<div class="modal-dialog" role="document">
+												<div class="modal-content">
+													<div class="modal-header">
+														<h5 class="modal-title" id="exampleModalLabel">Confirmation de suppression</h5>
+														<button type="button" class="close" data-dismiss="modal" aria-label="close">
+															<span aria-hidden="true">&times;</span>
+														</button>
+													</div>
+													<div class="modal-body">
+														<p>Êtes-vous sûr(e) de vouloir supprimer cet épisode ?</p>
+													</div>
+													<div class="modal-footer">
+														<button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+														<a href="index.php?action=delete_episode&amp;id=<?php echo $published_episode['id']; ?>" class="btn btn__admin">Confirmer</a>
+													</div>
+												</div>
+											</div>
+										</div>
+								<?php
+								}
+								?>
+							</ul> 
+	                	</article>
+	            	</li>                
+	        	<?php
+	       		}
+	        	?>
+				</ul>
+			</div>
+	    <?php
 	    }else{
 	    ?>
 	        <p class="episode__no">Pas d'épisode publié</p>
@@ -77,49 +79,51 @@ ob_start();
 	    if($nbepisode_inprogress > 0)
 	    {
 	    ?>
-	    	<ul> <!-- On affiche les épisodes en cours -->
-	    	<?php
-	        foreach ($inprogress as $inprogress_episode)
-	        {
-	        ?>
-	            <li>
-	                <article>
-	                    <p>Episode n°<?php echo $inprogress_episode['episode_number']; ?> :</p>
-	                    <h4><?php echo $inprogress_episode['episode_title']; ?></h4>
-	                    <ul>
-	                        <!-- Aperçu de l'épisode -->
-	                        <li><a href="index.php?action=look_episode&amp;id=<?php echo $inprogress_episode['id']; ?>" class="btn btn__admin">Aperçu</a></li>
-	                        <!-- Modifier l'épisode -->
-	                        <li><a href="index.php?action=update_episode&amp;id=<?php echo $inprogress_episode['id']; ?>" class="btn btn__admin">Modifier</a></li>
-	                        <!-- Supprimer l'épisode avec demande de confirmation -->
-	                        <li><button type="button" data-toggle="modal" data-target="#Modal2">Supprimer</button></li>
-	                            <!-- Modal -->
-	                            <div class="modal fade" id="Modal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-	                                <div class="modal-dialog" role="document">
-	                                    <div class="modal-content">
-	                                        <div class="modal-header">
-	                                            <h5 class="modal-title" id="exampleModalLabel">Confirmation de suppression</h5>
-	                                            <button type="button" class="close" data-dismiss="modal" aria-label="close">
-	                                                <span aria-hidden="true">&times;</span>
-	                                            </button>
-	                                        </div>
-	                                        <div class="modal-body">
-	                                            <p>Êtes-vous sûr(e) de vouloir supprimer cet épisode ?</p>
-	                                        </div>
-	                                        <div class="modal-footer">
-	                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
-	                                            <a href="index.php?action=delete_episode&amp;id=<?php echo $inprogress_episode['id']; ?>" class="btn btn__admin">Confirmer</a>
-	                                        </div>
-	                                    </div>
-	                                </div>
-	                            </div>
-	                    </ul> 
-	                </article>
-	            </li>
-	        <?php
-	        }
-	        ?>
-	    	</ul>
+			<div class="novel-episodes__list">
+				<ul> <!-- On affiche les épisodes en cours -->
+				<?php
+				foreach ($inprogress as $inprogress_episode)
+				{
+				?>
+					<li class="row">
+						<article class="col-md-8 col-sm-10 col-xs-12">
+							<p>Episode n°<?php echo $inprogress_episode['episode_number']; ?></p>
+							<h4><?php echo $inprogress_episode['episode_title']; ?></h4>
+							<ul class="novel-episodes__list__buttons">
+								<!-- Aperçu de l'épisode -->
+								<li><a class="btn btn__read" href="index.php?action=look_episode&amp;id=<?php echo $inprogress_episode['id']; ?>" class="btn btn__admin">Aperçu</a></li>
+								<!-- Modifier l'épisode -->
+								<li><a class="btn btn__read" href="index.php?action=update_episode&amp;id=<?php echo $inprogress_episode['id']; ?>" class="btn btn__admin">Modifier</a></li>
+								<!-- Supprimer l'épisode avec demande de confirmation -->
+								<li><button class="btn btn__alert" type="button" data-toggle="modal" data-target="#Modal2">Supprimer</button></li>
+									<!-- Modal -->
+									<div class="modal fade" id="Modal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+										<div class="modal-dialog" role="document">
+											<div class="modal-content">
+												<div class="modal-header">
+													<h5 class="modal-title" id="exampleModalLabel">Confirmation de suppression</h5>
+													<button type="button" class="close" data-dismiss="modal" aria-label="close">
+														<span aria-hidden="true">&times;</span>
+													</button>
+												</div>
+												<div class="modal-body">
+													<p>Êtes-vous sûr(e) de vouloir supprimer cet épisode ?</p>
+												</div>
+												<div class="modal-footer">
+													<button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+													<a href="index.php?action=delete_episode&amp;id=<?php echo $inprogress_episode['id']; ?>" class="btn btn__admin">Confirmer</a>
+												</div>
+											</div>
+										</div>
+									</div>
+							</ul> 
+						</article>
+					</li>
+				<?php
+				}
+				?>
+				</ul>
+			</div>
 	    	<?php
 	    }else{
 	    ?>
